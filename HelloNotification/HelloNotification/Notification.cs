700fddea4cf44ea7e9fd919047bdc7aad49326c9
Victor Ye <viye@microsoft.com>
2018-06-10 21:02:23 -0700

    notification hello world initial check in

---
+++ HelloNotification/HelloNotification/Notification.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Notifications;
+using Windows.Data.Xml.Dom;
+using Windows.Networking.PushNotifications;
+
+namespace HelloNotification
+{
+    public class Notification
+    {
+        private PushNotificationChannel channel;
+
+        public void Here()
+        {
+            int b = 3;
+            b++;
+        }
+
+        public async void CreateChannel()
+        {
+            try
+            {
+                this.channel = await PushNotificationChannelManager.CreatePushNotificationChannelForApplicationAsync();
+                int a = 3;
+                a++;
+            }
+
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
---
+++ NotificationServer/NotificationServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NotificationServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NotificationServer ns = new NotificationServer();
+            ns.Send();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
